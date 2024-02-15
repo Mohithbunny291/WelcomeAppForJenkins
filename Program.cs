6847687ee503ca2b8f08e9088b4cbd86d2d593dc
Mohith Reddy <Mohithbunny291@gmail.com>
2024-02-15 15:01:34 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using WelcomeAppForJenkins;
+
+Console.WriteLine("Please Authenticate yourself");
+Signin signin = new Signin();
+//string user = Console.ReadLine();
+//string password = Console.ReadLine();
+string isValid = signin.Authenticate("admin", "admin123");
+Console.WriteLine(isValid);
---
+++ Signin.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WelcomeAppForJenkins
+{
+    public class Signin
+    {
+        public string Authenticate(string username, string password)
+        {
+            string msg;
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                msg = "UserName and Password Required";
+            }
+            else
+            {
+                if ((username == "admin") && (password == "admin123"))
+                    msg = "Authentication Pass";
+                else
+                    msg = "Authentication Fail";
+            }
+
+            return msg;
+        }
+    }
+}
