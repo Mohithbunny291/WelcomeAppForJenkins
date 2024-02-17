64a3cebfd7e37fb520d95d105fe8f87f9df73274
Mohith Reddy <Mohithbunny291@gmail.com>
2024-02-17 13:04:23 +0530

    Changed to hardcoded

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ using WelcomeAppForJenkins;
 
 Console.WriteLine("Please Authenticate yourself");
 Signin signin = new Signin();
-string user = Console.ReadLine();
-string password = Console.ReadLine();
-string isValid = signin.Authenticate(user, password);
+//string user = Console.ReadLine();
+//string password = Console.ReadLine();
+string isValid = signin.Authenticate("admin", "admin1234");
 Console.WriteLine(isValid);
